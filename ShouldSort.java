35881011aeef2054456e804aeb8426d64681a8d5
kennyyu168 <kennyyu168@yahoo.com>
2019-02-13 20:08:04 -0800

    added another comment to test fetching

--- ShouldSort.java
+++ ShouldSort.java
@@ -3,6 +3,7 @@
 public class ShouldSort { 
 
 	// Added a comment for core branch
+	// Added a comment to test fetch
 	public static void main(String args[]) { 
 		System.out.println("Hello World");
 	} 
